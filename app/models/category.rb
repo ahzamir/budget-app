b5b9de84d7e60ee0c79c25e05ed91072e5052aad
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-19 18:12:17 +0430

    add validation for categories class model and added some more validations for user class model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
     belongs_to :user, class_name: "User"
     has_many :joins, dependent: :destroy, foreign_key: "category_id", class_name: "Join"
     has_many :expenses, through: :joins, source: :expense
+
+    validates :name, :icon, presence: true, length: { maximum: 50 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
     has_many :categories, dependent: :destroy
     has_many :expenses, dependent: :destroy
 
-    validates :name, :photo, presence: true, length: { maximum: 50 }
+    validates :name, :email, presence: true, length: { maximum: 50 }
+    validates :email, uniqueness: true
+    validates :password, presence: true, length: { minimum: 6 }
 end
