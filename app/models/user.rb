a1a5ff166ef83cc3e6257cb9e72e8456172f67a1
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-19 18:07:57 +0430

    add validation for user class model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :categories, dependent: :destroy
     has_many :expenses, dependent: :destroy
+
+    validates :name, :photo, presence: true, length: { maximum: 50 }
 end
