12b5a68f72e90fc43656f742874f540ad7a795e3
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-20 15:39:42 +0430

    solved the join table associations for categories and expenses classes

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :user, class_name: 'User'
-  has_many :joins, dependent: :destroy, foreign_key: 'expense_id', class_name: 'Join'
-  has_many :categories, through: :joins, source: :category
+  has_many :joins, dependent: :destroy, foreign_key: 'expenses_id', class_name: 'Join'
+  has_many :categories, through: :joins
 
   validates :name, :amount, presence: true, length: { maximum: 50 }
   validates :amount, numericality: { greater_than: 0 }
