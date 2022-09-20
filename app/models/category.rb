a3726a64e2ea766bc5444be5037d774b6779f164
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-20 15:17:12 +0430

    add the total expenses amount methode in the category model and added the name and icon of categories to the index page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,11 @@
 class Category < ApplicationRecord
   belongs_to :user, class_name: 'User'
   has_many :joins, dependent: :destroy, foreign_key: 'category_id', class_name: 'Join'
-  has_many :expenses, through: :joins, source: :expense
+  has_many :expenses, through: :joins, source: :expense, class_name: 'Expense'
 
   validates :name, :icon, presence: true, length: { maximum: 50 }
+
+  def total_expenses
+    expenses.where(user_id: user.id).sum(:amount)
+  end
 end
