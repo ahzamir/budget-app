67c2345916e509a032be9fcecf21e591b0b8d714
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-21 17:19:05 +0430

    write tests for invalid attributes for creating category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Category, type: :model do
     category = Category.new(name: 'Test Category', icon: 'test-icon', user_id: @user.id)
     expect(category).to be_valid
   end
+
+  it 'is not valid without a name' do
+    category = Category.new(name: nil, icon: 'test-icon', user_id: @user.id)
+    expect(category).to_not be_valid
+  end
+
+  it 'is not valid without an icon' do
+    category = Category.new(name: 'Test Category', icon: nil, user_id: @user.id)
+    expect(category).to_not be_valid
+  end
 end
