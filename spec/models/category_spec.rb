ba510bcff99b9f0bdac35986aff693588fa2ee08
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-21 17:18:01 +0430

    write test for checking the validation of vaild category class

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create!(name: 'Ahmad Zamir Yousufi', email: 'example@gmail.com', password: '123456')
+  end
+
+  it 'is valid with valid attributes' do
+    category = Category.new(name: 'Test Category', icon: 'test-icon', user_id: @user.id)
+    expect(category).to be_valid
+  end
 end
