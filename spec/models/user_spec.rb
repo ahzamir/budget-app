e12cc01f4396cc580a18eb9813bc3f74b073292f
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-21 17:30:11 +0430

    add some model validation tests for user class

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    user = User.new(name: 'Ahmad Zamir Yousufi', email: 'example@gmail.com', password: '123456')
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    user = User.new(name: nil, email: 'example@gmail.com', password: '123456')
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without an email' do
+    user = User.new(name: 'Ahmad Zamir Yousufi', email: nil, password: '123456')
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without a password' do
+    user = User.new(name: 'Ahmad Zamir Yousufi', email: 'example@gmail.com', password: nil)
+    expect(user).to_not be_valid
+  end
 end
