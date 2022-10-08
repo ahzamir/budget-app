db32fc561e9df0e0417fc97d9d0da70ddaab08ba
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-10-08 23:06:43 +0430

    created routes for welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'categories#index'
+  get 'welcome/index'
+  get 'welcome/info'
+  get 'welcome/start'
+
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
+    authenticated :user do
+      root 'categories#index', as: :authenticated_root
+    end
+    unauthenticated { root 'welcome#index', as: :unauthenticated_root }
   end
+  devise_for :users
   resources :users do
     resources :categories do
       resources :expenses
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :welcome, only: %i[index info start]
 end
+
